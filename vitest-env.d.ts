2a1b4d3d4ee261a96c253b393f3cdfa806175433
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-08-24 18:29:30 -0400

    test: update vitest environment
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- vitest-env.d.ts
+++ vitest-env.d.ts
@@ -2,14 +2,16 @@
 
 interface ImportMetaEnv extends Readonly<import('vite').ImportMetaEnv> {
   readonly BASE_URL: string
-  readonly CI?: 'false' | 'true'
+  readonly CI: 'false' | 'true'
   readonly DEV: '1' | import('@flex-development/tutils').EmptyString
-  readonly MODE: import('@flex-development/tutils').NodeEnv
+  readonly MODE: import('@flex-development/tutils').NodeEnv.TEST
+  readonly NODE_ENV: import('@flex-development/tutils').NodeEnv.TEST
   readonly PROD: '1' | import('@flex-development/tutils').EmptyString
   readonly SSR: '1' | import('@flex-development/tutils').EmptyString
   readonly TEST: 'true'
   readonly VITEST: 'true'
-  readonly VITE_USER_NODE_ENV: import('@flex-development/tutils').NodeEnv
+  readonly VITE_USER_NODE_ENV: import('@flex-development/tutils').NodeEnv.TEST
+  readonly VITEST_CLI_WRAPPER: 'false' | 'true'
   readonly VITEST_SEGFAULT_RETRY: import('@flex-development/tutils').Numeric
 }
 
